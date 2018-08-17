16ef168d7f8a41b27299fc11eba9b9a2347df78f
Peter Dunham <dunhampa@gmail.com>
2018-08-17 16:55:20 -0400

    updating based on whats in server

--- shiny_ohio/app.R
+++ shiny_ohio/app.R
@@ -13,13 +13,34 @@ ui <- fluidPage(
     tags$style(HTML(".leaflet-container { background: #fff; }"))
   ),
   tags$style(type = "text/css", "#myMap {height: calc(100vh - 80px) !important;}"),
-  leafletOutput("myMap")
+  
+  titlePanel("Hello Shiny!"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      sliderInput("obs", "Number of observations:",  
+                  min = 1, max = 1000, value = 500)
+    ),
+    
+    mainPanel(
+      leafletOutput("myMap")
+    )
+  
+  
+  
+  
+  )
+  
+  
+  
+ 
 )
 
 # Server logic
 server <- function(input, output) {
   
-  
+  birthdata<-read.csv("ReportOutput-BirthWeight.csv", stringsAsFactors = FALSE)
   
   
   
@@ -31,21 +52,29 @@ server <- function(input, output) {
     Statekey<-read.csv('STATEFPtoSTATENAME_Key.csv', colClasses=c('character'))
     states<-merge(x=states, y=Statekey, by="STATEFP", all=TRUE)
     
+    
     neStates <- subset(states, states$STATENAME %in% c(
       "Ohio"
     ))
     
-    states<-merge(x=states, y=Statekey, by="STATEFP", all=TRUE)
+    neStates<-sp::merge(x=neStates, y=birthdata, by.x="NAME", by.y="County", by=x)
+    
+   #Low.Birth<-as.numeric(birthdata$Low.Birth.Count)[1:88]
+   #neStates<-cbind(neStates,Low.Birth)
+    #states<-merge(x=states, y=Statekey, by="STATEFP", all=TRUE)
     map<-leaflet(neStates) %>%
       addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
                   opacity = 1.0, fillOpacity = 0.5,
                   layerId = ~NAME,
-                  fillColor = ~colorQuantile("Reds", AWATER)(AWATER),
+                  fillColor = ~colorQuantile("Reds",Low.Birth.Count )(Low.Birth.Count),
                   highlightOptions = highlightOptions(color = "white", weight = 2,
                                                       bringToFront = TRUE),
-                  popup=~NAME)
+                  popup = ~as.factor(paste0(neStates$NAME,",",neStates$Low.Birth.Count,",",neStates$Norm.Birth.Count)))
+
     print("here")
-    map
+    #setView(11, 49,  zoom = 9)
+    #map$center<-list(lng=-82.7, lat=35.7)
+    map<-map %>% setView(-82, 38.9,  zoom = 7)
   }
   
   output$myMap <- renderLeaflet({
@@ -56,7 +85,11 @@ server <- function(input, output) {
   
   observeEvent(input$myMap_shape_click, { # update the location selectInput on map clicks
     p <- input$myMap_shape_click
+    p2<-input$myMap_center
+    p3<-input$myMap_zoom
     print(p)
+    print(str(p2))
+    print(str(p3))
   }) 
   
   
